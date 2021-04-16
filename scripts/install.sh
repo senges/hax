0dfdd969a265aa7a147f0454cc87197edb421733
Charles SENGES <charles.senges.etu@univ-lille.fr>
2021-04-16 17:11:22 +0200

    fix(install.sh): use bash instead of sh for extended if support

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,4 +7,4 @@ fi
 
 echo "[+] Installing $1"
 
-sh -c "$(curl -kfsSL https://raw.githubusercontent.com/senges/hax/main/scripts/$1.sh)"
+bash -c "$(curl -kfsSL https://raw.githubusercontent.com/senges/hax/main/scripts/$1.sh)"
