d65a94f2d211e8d935d607e6bac7a23e40f46cf0
Charles SENGES <charles.senges.etu@univ-lille.fr>
2021-04-16 14:55:14 +0200

    fix(scripts): few minor corrections

--- scripts/dog.sh
+++ scripts/dog.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt install -y --no-install-recommends gnupg2
+
 # Add community apt repo
 echo "deb http://packages.azlux.fr/debian/ buster main" | tee /etc/apt/sources.list.d/azlux.list
 wget -qO - https://azlux.fr/repo.gpg.key | apt-key add - 
