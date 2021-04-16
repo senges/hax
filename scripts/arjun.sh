09d874ba809016d3aa844dbd5596923e7c2b7f17
Charles SENGES <charles.senges.etu@univ-lille.fr>
2021-04-16 14:47:12 +0200

    feat(scripts): add php, arjun, exiftool

---
+++ scripts/arjun.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+which pip || echo "Pip must be installed" ; exit 1
+
+pip install arjun
--- scripts/php.sh
+++ scripts/php.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 apt install -y --no-install-recommends php-cli php-curl php-json php-zip
-
