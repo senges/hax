556434ef56c0c7615de0a23d088fdb6786db6c23
Charles SENGES <charles.senges.etu@univ-lille.fr>
2021-04-18 17:06:27 +0200

    common(hax.py): working on expose feature

--- hax.py
+++ hax.py
@@ -19,10 +19,13 @@ EXIT_SUCCESS = 0
 EXIT_FAILURE = 1
 
 images = {
-    'legacy'    : 'hax',
-    'msf'       : 'hax-msf',
+    'legacy'    : 'hax-legacy',
+    'hax'       : 'hax',
     'minimal'   : 'hax-minimal',
-    'hashcat'   : 'hax-hashcat',
+    'expose'    : 'hax-expose',
+    'msf'       : 'hax-msf',
+    'cracking'  : 'hax-crack',
+    'osint'     : 'hax-osint'
 }
 
 verbose = True
@@ -30,52 +33,57 @@ verbose = True
 client = docker.from_env()
 console = Console()
 
-@click.group(invoke_without_command = True)
+@click.group(invoke_without_command=True)
 @click.help_option('-h', '--help')
 # @click.option('-v', '--verbose', 'vflag', is_flag = True, help = 'Verbose mode')
 @click.pass_context
-def main(ctx = None, image = False, vflag = False):
+def main(ctx=None, image=False, vflag=False):
     # verbose = vflag
 
     # If no parameter, just run the legacy container
     if ctx.invoked_subcommand is None:
-        spawn( 'legacy', False, () )
+        spawn('legacy', False, ())
+
 
 @main.command()
 @click.help_option('-h', '--help')
-@click.argument('name', required = False, default = 'legacy', type = click.STRING)
-@click.option('-p', '--pull',   is_flag = True,  help = 'Pull image if not present')
-@click.option('-v', '--volume', is_flag = False, multiple = True, help = 'Additional volume in docker format')
-def run(name, pull, volume):
+@click.argument('name', required=False, default='legacy', type=click.STRING)
+@click.option('-p', '--pull',   is_flag=True,  help='Pull image if not present')
+@click.option('-v', '--volume', is_flag=False, multiple=True, help='Additional volume in docker format')
+@click.option('-P', '--port',   is_flag=False, multiple=True, help='Port binding in docker format')
+def run(name, pull, volume, port):
     """Run ctf environment"""
-    
-    spawn( name, pull, volume )
+
+    spawn(name, pull, volume)
+
 
 @main.command()
 @click.help_option('-h', '--help')
-@click.argument('name', required = False, default = 'legacy', type = click.Choice( list(images) ))
+@click.argument('name', required=False, default='legacy', type=click.Choice(list(images)))
 def pull(name):
     """Pull image and exit"""
-    
-    dockerPull( name )
+
+    dockerPull(name)
+
 
 @main.command()
 @click.help_option('-h', '--help')
 def list():
     """List local images"""
 
-    table = Table( show_header = True, header_style = "bold", box = box.SQUARE )
+    table = Table(show_header=True, header_style="bold", box=box.SQUARE)
     table.add_column("Images")
     table.add_column("Size",  justify='right')
     table.add_column("State", justify='center')
 
     for i in images:
-        if img := dockerStat( images[i] ):
+        if img := dockerStat(images[i]):
             table.add_row(i, '[i]' + dockerSize(img), '✓')
         else:
             table.add_row(i, '-', '✗ ')
 
     console.print(table)
+
 
 @main.command()
 @click.help_option('-h', '--help')
@@ -83,60 +91,108 @@ def refresh():
     """Pull latest version of local images"""
 
     for i in images:
-        if dockerStat( images[i] ):
-            try: dockerPull( images[i] )
-            except: pass
+        if dockerStat(images[i]):
+            try:
+                dockerPull(images[i])
+            except:
+                pass
+
+
+@main.command()
+@click.help_option('-h', '--help')
+@click.argument('port', required = True, type = click.STRING)
+@click.option('-p', '--pull', is_flag = True, help='Pull image if not present')
+def expose(port, pull):
+    """
+    Expose TCP endpoint using ngrok
+
+    PORT is the local port you want to expose
+    """
+
+    spawn ( 
+        name    = 'expose', 
+        pull    = pull, 
+        volumes = (), 
+        ports   = (f'{ port }:{ port }', '4242:4242'), 
+        env     = [ 'PORT=' + port ]
+    )
 
 @main.command()
 @click.help_option('-h', '--help')
 def wordlist():
     """Manage wordlists"""
 
-    panic( 'Not yet implemented' )
+    panic('Not yet implemented')
+
 
 @main.command()
 @click.help_option('-h', '--help')
 def build():
     """Build images from source"""
 
-    panic( 'Not yet implemented' )
+    panic('Not yet implemented')
 
 # Spawn new docker environment
-def spawn(name: str, pull: bool = False, volume: tuple = ()):
+def spawn(name: str, pull: bool = False, volumes: tuple = (), ports: tuple = (), env: list = []):
 
     # Pull if required
-    if pull: dockerPull( images[name] )
+    if pull:
+        dockerPull(images[name])
 
     # Generate volumes between host and docker
-    volumes = dockerVolumes(volume)
+    volumes = dockerVolumes(volumes)
+
+    # Generate ports bindings between host and docker
+    ports = dockerPorts(ports)
 
     # Run image
-    dockerRun(images[name], volumes)
+    dockerRun(images[name], volumes, ports, env)
 
 # Check if local image is present
 def dockerStat(name: str):
     try:
-        img = client.images.get( name )
+        img = client.images.get(name)
 
     except docker.errors.ImageNotFound:
         return False
 
     except docker.errors.APIError:
-        panic( 'Could not connect to docker socket' )
+        panic('Could not connect to docker socket')
 
     return img
+
+# Docker port list generation
+def dockerPorts(userPort: tuple = ()) -> dict:
+    ports = {}
+
+    for p in userPort:
+        try:
+            chunk = p.split(':')
+
+            if len(chunk) < 2:
+                raise ValueError('Missing parts in port declaration')
+        
+            ports[ chunk[1] ] = chunk[0]
+
+        except ValueError as err:
+            panic(err.args[0])
+
+        except:
+            panic(f'Invalid port format `{ p }`')
+
+    return ports
 
 # Docker volume list generation
 def dockerVolumes(userVolumes: tuple = ()) -> dict:
     volumes = {
-        '/tmp' : { 'bind' : '/tmp', 'mode' : 'rw' },
+        '/tmp': {'bind': '/tmp', 'mode': 'rw'},
     }
 
     # If current user has ssh, share read only
     ssh = '%s/.ssh' % os.environ.get('HOME')
 
     if os.path.isdir(ssh):
-        volumes[ ssh ] = { 'bind' : '/root/.ssh', 'mode' : 'ro' }
+        volumes[ssh] = {'bind': '/root/.ssh', 'mode': 'ro'}
 
     # Add user custom volume if any
     for v in userVolumes:
@@ -145,81 +201,87 @@ def dockerVolumes(userVolumes: tuple = ()) -> dict:
 
             if len(chunk) < 2:
                 raise ValueError('Missing parts in volume declaration')
-            
+
             # Default mount strategie is read/write
             if len(chunk) == 2:
                 chunk.append('rw')
 
             # Not sure if necessary as docker does not enforce
             # local path stat.
-            if not os.path.isdir( chunk[0] ):
-                raise ValueError( f'Path { chunk[0] } does not exist' )
+            if not os.path.isdir(chunk[0]):
+                raise ValueError(f'Path { chunk[0] } does not exist')
 
             # Make sure rwx rights are properly set
             if chunk[2] not in ['ro', 'rw']:
-                raise ValueError( 'Volume rights must be one of : [ro, rw]')
+                raise ValueError('Volume rights must be one of : [ro, rw]')
 
-            volumes[ chunk[0] ] = { 'bind' : chunk[1], 'mode' : chunk[2] }
-            
+            volumes[chunk[0]] = {'bind': chunk[1], 'mode': chunk[2]}
+
         except ValueError as err:
-            panic( err.args[0] )
+            panic(err.args[0])
 
         except:
-            panic( f'Invalid volume format `{ v }`' )
+            panic(f'Invalid volume format `{ v }`')
 
     return volumes
 
 # Pull docker image
 def dockerPull(name: str):
 
-    info ( f'Pulling image { name } from registry' )
+    info(f'Pulling image { name } from registry')
 
     try:
         with console.status("[bold grey]Pulling ..."):
-            client.images.pull( name, 'latest' )
+            client.images.pull(name, 'latest')
 
     except docker.errors.APIError:
-        panic( f'Could not pull image { name }' )
+        panic(f'Could not pull image { name }')
 
-    success( f'Image { name } pull done' )
+    success(f'Image { name } pull done')
 
 # Start container
-def dockerRun(name: str, volumes: dict = {}):
+def dockerRun(name: str, volumes: dict = {}, ports: dict = {}, env: list = []):
 
-    if not dockerStat( name ):
-        panic( f'Image { name } not found locally. Use `pull` command or add `--pull` flag.' )
+    if not dockerStat(name):
+        panic(
+            f'Image { name } not found locally. Use `pull` command or add `--pull` flag.')
 
     container = client.containers.create(
-        image           = '%s:latest' % name,
-        auto_remove     = True,
-        hostname        = 'hax',
-        stdin_open      = True,
-        tty             = True,
-        volumes         = volumes
+        image='%s:latest' % name,
+        auto_remove=True,
+        hostname='hax',
+        stdin_open=True,
+        tty=True,
+        volumes=volumes,
+        ports=ports,
+        environment=env,
     )
 
     dockerpty.start(client.api, container.id)
 
 # Return docker image size
 def dockerSize(image: str) -> str:
-    return prettySize( image.attrs['Size'] )
+    return prettySize(image.attrs['Size'])
 
 # Program panic
 def panic(err: str):
     console.print('[red]✗ ' + err)
-    exit( EXIT_FAILURE )
+    exit(EXIT_FAILURE)
 
 # Display cli info
 def info(msg: str):
-    if verbose: console.print('[yellow]~ ' + msg)
+    if verbose:
+        console.print('[yellow]~ ' + msg)
 
 # Display cli info
 def success(msg: str):
-    if verbose: console.print('[green]✓ ' + msg)
+    if verbose:
+        console.print('[green]✓ ' + msg)
 
 # Pretty print bytes
-def prettySize(bytes, units=[ '','KO','Mo','Go','To' ]):
-    return str(bytes) + ' ' + units[0] if bytes < 1024 else prettySize( bytes>>10, units[1:] )
+def prettySize(bytes, units=['', 'KO', 'Mo', 'Go', 'To']):
+    return str(bytes) + ' ' + units[0] if bytes < 1024 else prettySize(bytes >> 10, units[1:])
+
 
 if __name__ == '__main__':
     main()
