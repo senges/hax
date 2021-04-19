2e7f049a9d754de61f0856668ab6ba1ce54bc1d9
Charles SENGES <charles.senges.etu@univ-lille.fr>
2021-04-19 15:19:32 +0200

    feat(pydictor.sh): remove tmp fix as my pr was merged on officiel repo

--- scripts/pydictor.sh
+++ scripts/pydictor.sh
@@ -2,11 +2,6 @@
 
 git clone --depth=1 --branch=master https://github.com/landgrey/pydictor.git /tools/pydictor
 
-# Waiting for my PR to be merged on the official repo.
-# This is a temporary patch.
-sed -i '21s/abspath/realpath/' /tools/pydictor/lib/data/data.py
-sed -i '22s/abspath/realpath/' /tools/pydictor/lib/data/data.py
-
 # Add to PATH
 ln -s /tools/pydictor/pydictor.py /tools/bin/pydictor
 
